8885b7e6597ff8111fb14757802ba908035175b2
mankal111 <mankal111@hotmail.com>
2016-01-10 23:10:56 +0200

    Changed layout

--- myui.js
+++ myui.js
@@ -4,7 +4,7 @@ $( "#new-point-btn" ).click(function(){
   points++;
   $( "#accordion" ).append('<div class="panel panel-default" id="panel'+points+'"><div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" href="#collapse'+points+'">Point '+points+'</a></h4></div><div id="collapse'+points+'" class="panel-collapse collapse"><div class="panel-body"><a id="new-pair-btn'+points+'" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-plus"></span>Add angle-point pair</a><a id="del-pair-btn'+points+'" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-minus"></span>Delete pair</a></div></div></div>');
   $( "#new-pair-btn"+points+"" ).click(function(){
-    $(this).parents("div:first").before('<form class="form-inline" role="form"><div class="form-group"><label for="angle"><h5>Angle:</label><input type="text" class="form-control" id="angle" size="5">π</h5></div>&nbsp;<div class="form-group"><label for="point"><h5>Points to point:</label><input type="number" class="form-control" id="point" maxlength="4"></h5></div></form>');
+    $(this).parents("div:first").before('<form class="form-inline" role="form"><div class="form-group"><label for="angle"><h5>Angle:</label><input type="text" class="form-control" id="angle" size="5">π</h5></div>&nbsp;<div class="form-group"><label for="point"><h5>Point:</label><input type="number" class="form-control" id="point" maxlength="4" style="width:100px"></h5></div></form>');
   });
 
   $( "#del-pair-btn"+points+"" ).click(function(){
