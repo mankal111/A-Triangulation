5837a17bb35f4fc1ed2e14debce284eddbc8b992
mankal111 <mankal111@hotmail.com>
2016-01-10 11:10:40 +0200

    created buttons in panels

--- myui.js
+++ myui.js
@@ -2,7 +2,7 @@ var points = 0;
 
 $( "#new-point-btn" ).click(function(){
   points++;
-  $( "#accordion" ).append('<div class="panel panel-default" id="panel'+points+'"><div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" href="#collapse'+points+'">Point '+points+'</a></h4></div><div id="collapse'+points+'" class="panel-collapse collapse"><div class="panel-body">Panel Body</div><div class="panel-footer">Panel Footer</div></div></div>');
+  $( "#accordion" ).append('<div class="panel panel-default" id="panel'+points+'"><div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" href="#collapse'+points+'">Point '+points+'</a></h4></div><div id="collapse'+points+'" class="panel-collapse collapse"><div class="panel-body"><a id="new-pair-btn" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-plus"></span>Add angle-point pair</a><a id="del-pair-btn" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-minus"></span>Delete pair</a></div></div></div>');
 });
 
 $( "#del-point-btn" ).click(function(){
